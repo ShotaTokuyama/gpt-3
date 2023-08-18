d803847a8c3150c7e68ea16ab3684863411e6e2b
tokuyama_shota <tokush1031@gmail.com>
2023-08-18 14:54:17 +0900

    first-commit

---
+++ server/api/generate.post.ts
@@ -0,0 +1,32 @@
+// 1 フロント側から渡されたリクエストデータを取得
+// 2 OpenAIのAPIに渡すpayloadを作成
+// 3 fetchメソッドでOpenAIからレスポンスオブジェクトを取得
+// 4 レスポンスオブジェクトからjsonデータを取得しフロントに返す
+
+export default defineEventHandler(async (event) => {
+  // リクエストデータの body を取得
+  const { prompt } = await readBody(event);
+
+  const payload = {
+    model: "text-davinci-003",
+    prompt,
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 300,
+    n: 1,
+  };
+
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  });
+
+  const json = await response.json();
+  return json;
+});
